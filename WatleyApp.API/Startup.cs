21511d309cd4ff309180c6f0894d817fad57da93
Watley <watlinton@hotmail.com>
2020-02-25 20:53:34 +0200

    - Added SQLite

--- WatleyApp.API/Startup.cs
+++ WatleyApp.API/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WatleyApp.API.Data;
 
 namespace WatleyApp.API
 {
@@ -27,6 +29,7 @@ namespace WatleyApp.API
         {
             services.AddControllers();
             services.AddCors();
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
